5b63c7373bc44a632b77ca9b21e802d789189c54
arshiarezaeei <arshiarezaei9090@gmail.com>
2025-02-23 16:21:39 +0330

    Add files via upload

---
+++ functionhomework/functionSample/script.js
@@ -0,0 +1,10 @@
+function redius( rParameter ) {
+    return rParameter * rParameter * Math.PI;
+}
+console.log("result is" + ":" +redius(50));
+// Mapping
+
+function greeting( fname, lname) {
+    return "Hi! "+ fname + " " + lname;
+}
+console.log(greeting("Omid","Lak", "Mazaheri"));
